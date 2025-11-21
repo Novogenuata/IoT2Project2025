a5e110b329a2baee89e14180fed338f210c3f2b7
VJMFK <LE2384668@crc-lennox.qc.ca>
2025-11-21 13:19:19 -0500

    testing soil moisture sensor

---
+++ soilmoist.py
@@ -0,0 +1,22 @@
+from gpiozero import MCP3008
+from time import sleep
+
+moisture = MCP3008(channel=0)
+
+while True:
+    value = moisture.value          # 0.0 to 1.0
+    raw = int(value * 1023)
+
+    raw = 1023 - raw   # reverse output so air = dry, water = wet
+
+    print(f"Analog value: {raw}")
+
+    if raw < 300:
+        print("Soil is VERY WET")
+    elif raw < 700:
+        print("Soil is MOIST")
+    else:
+        print("Soil is DRY")
+
+    print("-------------------------")
+    sleep(1)
